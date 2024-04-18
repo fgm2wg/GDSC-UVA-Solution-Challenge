f48aefea2bca408c56004843520b6ba65dea5f0b
fgm2wg <fgm2wg@virginia.edu>
2024-04-18 15:15:15 -0400

    format

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-
 class Donation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     donor = db.Column(db.String(100))
